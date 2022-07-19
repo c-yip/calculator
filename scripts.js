ba4b4b0636165ff85a4bc5979e7f11f1d10933fa
Christian <christianyip92@gmail.com>
2022-07-19 14:51:03 -0700

    Start basic math operator functions.

---
+++ scripts.js
@@ -0,0 +1,17 @@
+//math functions
+
+const add = function(a) {
+    return a + a;
+}
+
+const subtract = function(a) {
+    return a - a;
+}
+
+const multiply = function(a) {
+    return a * a;
+}
+
+const divide = function(a) {
+    return a / a;
+}
