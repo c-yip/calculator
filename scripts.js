7aeb626c441938ed55dc8c53001c344bb46c340a
Christian <christianyip92@gmail.com>
2022-07-25 09:24:47 -0700

    Add numbers to display event listener.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
 const calculator = document.querySelector('.calculator');
 const display = calculator.querySelector('.display');
-const opDisplay = calculator.querySelector('.op-display')
+const opDisplay = calculator.querySelector('.op-display');
+const buttons = calculator.querySelector('.buttons')
+const numbers = calculator.querySelectorAll('.numbers');
+const operator = calculator.querySelectorAll('.operator');
+const num = calculator.querySelectorAll('[data-num]');
+const op = calculator.querySelectorAll('[data-op]');
+let firstNum;
+let secondNum;
+
+//event listeners
+numbers.forEach(number => {
+    number.addEventListener('click', e =>{
+        if (display.textContent == '0') {
+            display.textContent = e.target.textContent;
+        } else {
+            display.textContent += e.target.textContent;
+        }
+    })
+})
 
 
 
